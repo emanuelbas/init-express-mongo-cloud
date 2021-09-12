997802acd2f0390a3fb4d91d7822319f7ffb1574
emanuelbas <emanuelbastons@gmail.com>
2021-09-12 18:35:55 -0300

    endpoints publicas y privadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -27,6 +27,77 @@ router.post('/ingresar', async (req, res) => {
     return res.status(200).json({token});
 })
 
+router.get('/tareas', (re, res) => {
+    res.json([
+        {
+            _id: 1,
+            name: 'tarea 1',
+            descripcion: 'lorem imsum',
+            date: '2021-09-12T08:20:00.043+00:00'
+        },
+        {
+            _id: 2,
+            name: 'tarea 2',
+            descripcion: 'lorem imsum',
+            date: '2021-09-12T08:20:00.043+00:00'
+        },
+        {
+            _id: 3,
+            name: 'tarea 3',
+            descripcion: 'lorem imsum',
+            date: '2021-09-12T08:20:00.043+00:00'
+        },
+    ]);
+
+    
+});
+
+router.get('/tareas-privadas', verifyToken, (re, res) => {
+    res.json([
+        {
+            _id: 1,
+            name: 'tarea 1',
+            descripcion: 'lorem imsum',
+            date: '2021-09-12T08:20:00.043+00:00'
+        },
+        {
+            _id: 2,
+            name: 'tarea 2',
+            descripcion: 'lorem imsum',
+            date: '2021-09-12T08:20:00.043+00:00'
+        },
+        {
+            _id: 3,
+            name: 'tarea 3',
+            descripcion: 'lorem imsum',
+            date: '2021-09-12T08:20:00.043+00:00'
+        },
+    ]);
+
+    
+});
+
+router.get('/perfil', verifyToken, (req, res) => {
+    res.send(req.userId);
+})
+
 router.get('/', (req, res) => res.send('Hola!'))
 
 module.exports = router;
+
+function verifyToken(req, res, next){
+    if (!req.headers.authorization){
+        return res.status(401).send('Solicitud no autorizada')
+    }
+
+    const token =req.headers.authorization.split(' ')[1];
+    if (token === 'null'){
+        return res.status(401).send('Solicitud no autorizada');
+    }
+
+    const payload = jwt.verify(token, 'secretKey');
+    
+    req.userId = payload._id;
+    next();
+
+}
